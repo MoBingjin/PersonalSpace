011d28298e3b7e4a354987e808775d02e813f0ce
Mo <mo@mobingc.ml>
2022-06-08 00:05:28 +0800

    初始化

--- src/components/mo-main.vue
+++ src/components/mo-main.vue
@@ -1,9 +1,27 @@
 <template>
-    <div>主页</div>
+  <div v-html="html"></div>
 </template>
 
 <script>
+import showdown from "../plugins/unpkg.com/showdown@2.1.0/dist/showdown.min.js";
+
 const { defineComponent } = Vue;
 
-export default defineComponent({});
+export default defineComponent({
+  data() {
+    return {
+      html: "",
+    };
+  },
+  created() {
+    fetch("https://markdown.mobingc.ml/static/README.md")
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {
+        const converter = new showdown.Converter();
+        this.html = converter.makeHtml(data);
+      });
+  },
+});
 </script>
