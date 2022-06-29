d12dd1db36de4dee009e96ad106b134ccf3f7963
Mo <mo@mobingc.ml>
2022-06-30 00:12:29 +0800

    初始化

--- src/home/App.vue
+++ src/home/App.vue
@@ -1,24 +1,37 @@
 <template>
-  <div>
-    <div :class="headerClass">
-      <mo-head-menu></mo-head-menu>
-    </div>
-    <div>
-      <router-view></router-view>
-    </div>
+  <div :class="homeAppClass">
+    <span :class="sideMenuClass">
+      <mo-side-menu @close-side-menu="sideMenuStatus = false"></mo-side-menu>
+    </span>
+    <span :class="mainContainerClass">
+      <div :class="sideMenuBtnClass" @click="sideMenuStatus = !sideMenuStatus">
+        <e-icon v-if="sideMenuStatus" class="mo-side-menu-btn-icon" icon-name="fa fa-remove" />
+        <e-icon v-if="!sideMenuStatus" class="mo-side-menu-btn-icon" icon-name="fa fa-navicon" />
+      </div>
+      <div :class="hiddenAreaClass" @click="sideMenuStatus = !sideMenuStatus"></div>
+      <div :class="headerClass">
+        <mo-head-menu></mo-head-menu>
+      </div>
+      <div>
+        <router-view></router-view>
+      </div>
+    </span>
   </div>
 </template>
 
 <script setup>
 
 import MoHeadMenu from "./mo-head-menu.vue";
-const { getCurrentInstance, ref, watch } = window["Vue"];
+import MoSideMenu from "./mo-side-menu.vue";
+const { computed, getCurrentInstance, ref, watch } = window["Vue"];
 const { createRouter, createWebHashHistory } = window["VueRouter"];
 const { title } = window["MoConfig"].params;
 
 
 // 导航栏class
 const headerClass = ref("mo-hidden");
+// 侧边栏打开状态
+const sideMenuStatus = ref(false);
 
 
 // 路由规则
@@ -48,6 +61,13 @@ const routes = [
     component: () => import("../common/components/404.vue")
   }
 ];
+
+
+const homeAppClass = computed(() => sideMenuStatus.value ? "mo-home-app open" : "mo-home-app");
+const sideMenuClass = computed(() => sideMenuStatus.value ? "mo-side-menu open" : "mo-side-menu");
+const mainContainerClass = computed(() => sideMenuStatus.value ? "mo-main-container open" : "mo-main-container");
+const sideMenuBtnClass = computed(() => sideMenuStatus.value ? "mo-side-menu-btn open" : "mo-side-menu-btn");
+const hiddenAreaClass = computed(() => sideMenuStatus.value ? "mo-hidden-area open" : "mo-hidden-area");
 
 
 /**
@@ -84,6 +104,7 @@ const handleScroll = event => {
     // 404
     if (route.matched[0].path === "/:other") {
       window["document"].getElementsByTagName("title")[0].innerHTML = title["404"];
+      window["document"].getElementsByClassName("mo-side-menu-btn")[0].style["display"] = "none";
       headerClass.value = "mo-hidden";
       window.removeEventListener("scroll", handleScroll);
       return;
@@ -109,9 +130,80 @@ const handleScroll = event => {
 
 <style scoped>
 @media screen and (max-width: 960px) {
+
+  .mo-home-app.open {
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    position: fixed;
+  }
+
+  .mo-side-menu {
+    width: 0;
+    height: 100%;
+    max-width: 250px;
+    position: absolute;
+    display: none;
+    background-color: #fff;
+    transition: all .3s ease;
+    z-index: 999;
+  }
+
+  .mo-side-menu.open {
+    display: block;
+    width: 250px;
+  }
+
+  .mo-main-container {
+    width: 100%;
+    display: block;
+    padding-left: 0;
+    transition-duration: .5s;
+  }
+
+  .mo-main-container.open {
+    padding-left: 250px;
+  }
+
+  .mo-side-menu-btn {
+    width: 50px;
+    height: 50px;
+    position: absolute;
+    text-shadow: 2px 2px 10px #000;
+    z-index: 2;
+  }
+
+  .mo-side-menu-btn-icon {
+    width: 100%;
+    height: 100%;
+    font-size: 40px;
+    text-align: center;
+    line-height: 50px;
+    color: #ffffff;
+  }
+
+  .mo-hidden-area.open {
+    width: 100%;
+    height: 100vh;
+    display: block;
+    position: absolute;
+    background-color: #99999957;
+    z-index: 1;
+  }
+
   .mo-header {
     display: none;
   }
+
+}
+
+@media screen and (min-width: 961px) {
+
+  .mo-side-menu,
+  .mo-side-menu-btn {
+    display: none;
+  }
+
 }
 
 .mo-header {
@@ -120,7 +212,7 @@ const handleScroll = event => {
   height: 60px;
   top: 0;
   background-color: rgba(255, 255, 255, 0.95);
-  z-index: 9999;
+  z-index: 998;
 }
 
 .mo-header:hover {
---
+++ src/home/mo-side-menu.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="mo-side">
+        <span>{{ logo }}</span>
+        <span>
+            <el-menu :ellipsis="false" :router="true">
+                <el-menu-item v-for="item in menus" :index="item.index" @click="closeSideMenu">
+                    <e-icon :icon-name="item.icon" />
+                    <span>{{ item.name }}</span>
+                </el-menu-item>
+            </el-menu>
+        </span>
+    </div>
+</template>
+
+<script setup>
+
+const { reactive } = window["Vue"];
+const { title } = window["MoConfig"].params;
+
+
+// 回调对象
+const emits = defineEmits(["close-side-menu"]);
+
+
+// logo
+const logo = title["home"];
+// 菜单项
+const menus = reactive([
+    {
+        index: "/home",
+        icon: "el-icon-s-home",
+        name: "首页"
+    },
+    {
+        index: "/archives",
+        icon: "el-icon-s-cooperation",
+        name: "归档"
+    },
+    {
+        index: "/about",
+        icon: "el-icon-info",
+        name: "关于"
+    }
+]);
+
+
+/**
+ * 关闭侧边栏
+ */
+const closeSideMenu = () => emits("close-side-menu", {});
+
+</script>
+
+<style scoped>
+.mo-side {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+}
+
+.mo-side>span:first-child {
+    font-family: 'HYWenHei-85W', 'Merriweather Sans', Helvetica, Tahoma, Arial, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft Yahei', 'WenQuanYi Micro Hei', 'sans-serif';
+    color: #464646;
+    font-size: 28px;
+    font-weight: 800;
+    padding-top: 200px;
+    padding-bottom: 50px;
+}
+
+.el-menu {
+    --el-font-size-base: 16px;
+    --el-menu-bg-color: #ffffff00;
+    --el-menu-text-color: #666666;
+    --el-menu-hover-text-color: #666666;
+    --el-menu-active-color: #ffd04b;
+    --el-color-primary-light-9: #ffffff00;
+    border-right: none;
+    user-select: none;
+}
+</style>
